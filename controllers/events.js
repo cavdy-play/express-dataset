2b6de3c5c2d8dc8a82db65393a5b9ac0e9eddea1
Cavdy <cavdyofficials@gmail.com>
2020-02-26 21:55:44 +0100

    events

--- controllers/events.js
+++ controllers/events.js
@@ -1,23 +1,74 @@
 var db = require('../config/queries')
+var resStruture = require('../helpers/resStructure')
 
-var getAllEvents = () => {
-	db.findAll('*', 'event_id', function(res) {
-		console.log(res);
+var getAllEvents = (req, res) => {
+	db.findAll('*', 'event_id', function(err, rows) {
+		if (err) res.status(400).json();
+
+		const data = []
+		if (rows.length !== 0) {
+			rows.map(row => {
+				const obj = resStruture(row)
+				data.push(obj)
+			})
+		}
+		res.status(200).json(data);
 	})
 };
 
-var addEvent = () => {
+var addEvent = (req, res) => {
+	const {
+		id, type,
+		actor:{
+			id: actor_id, login, avatar_url
+		},
+		repo: {
+			id: repo_id, name, url
+		},
+		created_at
+	} = req.body;
 
+	db.find('event_id', 'event_id', function(err, rows) {
+		if (err) res.status(400).json();
+
+		if (rows.length !== 0 && rows[0].event_id === id) res.status(400).json();
+
+		db.insert(
+			('event_id, type, actor_id, login, avatar_url, repo_id, name, url, created_at'),
+			([id, type, actor_id, login, avatar_url, repo_id, name, url, created_at]),
+			function(err) {
+				if (err) res.status(400).json();
+	
+				res.status(201).json();
+			}
+		)
+	})
 };
 
 
-var getByActor = () => {
+var getByActor = (req, res) => {
+	const {params: {actorId}} = req;
+	db.find('*', 'actor_id', actorId, function(err, rows) {
+		if (err) res.status(404).json();
 
+		const data = []
+		if (rows.length !== 0) {
+			rows.map(row => {
+				const obj = resStruture(row)
+				data.push(obj)
+			})
+		}
+		res.status(200).json(data);
+	})
 };
 
 
-var eraseEvents = () => {
+var eraseEvents = (req, res) => {
+	db.remove('events', function(err) {
+		if (err) res.status(404).json();
 
+		res.status(200).json()
+	})
 };
 
 module.exports = {
--- routes/actor.js
+++ routes/actor.js
@@ -3,8 +3,8 @@ var router = express.Router();
 var actorController = require('../controllers/actors');
 
 // Routes related to actor.
-router.put('/', actorController.updateActor);
-router.get('/', actorController.getAllActors);
-router.get('/streak', actorController.getStreak);
+// router.put('/', actorController.updateActor);
+// router.get('/', actorController.getAllActors);
+// router.get('/streak', actorController.getStreak);
 
 module.exports = router;
