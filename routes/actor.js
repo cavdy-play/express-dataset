9ad4e58da4db7197a2f7c7b2ebdef9247040102f
Cavdy <cavdyofficials@gmail.com>
2020-02-26 12:49:29 +0100

    setup routes

--- routes/actor.js
+++ routes/actor.js
@@ -1,6 +1,10 @@
 var express = require('express');
 var router = express.Router();
+var actorController = require('../controllers/actors');
 
 // Routes related to actor.
+router.put('/', actorController.updateActor);
+router.get('/', actorController.getAllActors);
+router.get('/streak', actorController.getStreak);
 
 module.exports = router;
